a1d059915dd11fd4a06b4ba41a9109f24a575d4b
Jorge Orrantia <joldigital@yahoo.com>
2014-01-04 16:23:09 -0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
 
-   before do
+  before do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
@@ -14,6 +14,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   
   it { should be_valid }
 
@@ -95,6 +97,12 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+    
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+  end
+    
   end
 
   
